0b24d4aa40283b7bbdd9be533c4130536b483e0b
josefe17 <josefe17@gmail.com>
2019-02-16 15:22:57 +0100

    Firmware for analog controlling from AN0 (linear power).

--- main.c
+++ main.c
@@ -26,20 +26,40 @@ ISR(TIMER1_CAPT_vect)
 	zeroCrossingISR();
 }
 
+void InitADC(void)
+{
+	// Select Vref=AVcc & Left justified
+	ADMUX |= (1<<REFS0) | (1<<ADLAR);
+	//set prescaler to 128 and enable ADC
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+}
+
+unsigned char ReadADC(unsigned char ADCchannel)
+{
+	//select ADC channel with safety mask
+	ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+	//single conversion mode
+	ADCSRA |= (1<<ADSC);
+	// wait until ADC conversion is complete
+	while( ADCSRA & (1<<ADSC) );
+	return ADCH;
+}
+
+
 int main (void)
 {
 	//DDRB|=(1<<2);
 	//PORTB&=~(1<<2);
+
 	dimmer_init();
 	sei();
-	setFiringAngle(150);
-
+	InitADC();
 	while(1)
 	{
-		setFiringAngle(15000);
-		_delay_ms(500);
-		setFiringAngle(5000);
-		_delay_ms(500);
+
+		setDimmingValue(ReadADC(0));
+		_delay_ms(30);
+		//_delay_ms(5000);
 	}
 
 
